03fefd277c6f9cabd753b7cf86b9dff4e1225fe5
Atishay Jain <atishay.jain@rtctek.global>
2026-01-13 16:46:14 +0530

    javscript fundamentals

--- index.js
+++ index.js
@@ -37,8 +37,73 @@
 
 // };
 // obj.show();
-let button = document.getElementById("btn");
-let heading = document.getElementById("title");
-button.addEventListener("click", ()=>{
-    heading.textContent = " hello atishay";
-} )
+// let button = document.getElementById("btn");
+// let heading = document.getElementById("title");
+// button.addEventListener("click", ()=>{
+//     heading.textContent = " hello atishay";
+// } ) 
+
+// let b = 6;
+// function show(){
+//     let a = 6;
+//     return a;
+// }
+// let result=show();
+// console.log(result);
+// let obj = {
+//     name : "atishay",
+//     age : 18,
+//     height : 5
+// };
+// let obj1= {...obj};
+// console.log(obj);
+// obj1.name="irshad";
+// console.log(obj1);
+
+// let obj = {
+//     name : "atishay",
+//     age : 18,
+//     height : 5,
+//     address:{
+//         name1: " hello",
+//         age1:2
+//     } 
+// };
+// let obj1= {...obj};
+// console.log(obj);
+// obj1.address.name1="irshad";
+// console.log(obj);
+// console.log(obj1);
+
+
+// let obj = {
+//   name: "Atishay",
+//   address: {
+//     city: "Delhi",
+//     pin: 110001
+//   }
+// };
+
+// let copy = structuredClone(obj);
+
+// copy.address.city = "Mumbai";
+
+// console.log(obj.address.city);  // Delhi ✅
+// console.log(copy.address.city); // Mumbai
+
+// let a = "5";
+// let b =5;
+//  if(a===b){
+//     console.log("true");
+//  }else{
+//     console.log("false");
+//  }
+
+
+// let a =5;
+// ++a;
+// console.log(a);
+
+// let n = 7;
+// let result = n%2===0 ? " even " : "odd";
+// console.log(result);
