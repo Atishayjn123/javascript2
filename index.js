d5b7212c73e9ce7061b803b5dc75e2b685659312
Atishay Jain <atishay.jain@rtctek.global>
2026-01-14 17:23:34 +0530

    javascript

--- index.js
+++ index.js
@@ -71,7 +71,7 @@
 // };
 // let obj1= {...obj};
 // console.log(obj);
-// obj1.address.name1="irshad";
+// obj1.name="irshad";
 // console.log(obj);
 // console.log(obj1);
 
@@ -107,3 +107,98 @@
 // let n = 7;
 // let result = n%2===0 ? " even " : "odd";
 // console.log(result);
+
+// const arr = [1,23,4,5];
+// const newarr= arr.map((i)=>{
+//      return 2*i;
+    
+//  });
+//  console.log(arr);
+//  console.log(newarr);
+
+//  const newarr2= arr.forEach((i)=>{
+//      return 2*i;
+    
+//  });
+//  console.log(newarr2);
+
+// const arr=[
+//     {
+//         name:"atishay",
+//         age:10
+
+//     },
+//     {
+//         name:"vansh",
+//         age:10
+//     },
+//     {
+//         name:"hello",
+//         age: 12
+
+//     }
+// ];
+// const result=(arr[2].name);
+// console.log(result); 
+
+// for(let i =0;i<=5;i++){
+//     console.log(i*2);
+// }
+// let i =1;
+// do{
+//     console.log(i*2);
+//     i++;
+
+// }while(i<=5)
+// let arr=[1,2,3,4,5];
+// let target =3;
+// function linearSearch(arr,target){
+//     for(let i =0;i<=arr.length;i++){
+//         if(arr[i]===target){
+//             return i;
+//         }
+//     }
+//     return -1;
+
+// }
+
+// console.log(linearSearch(arr,target));
+
+// let obj1={
+//     name:"atishay",
+//     show(){
+//        let b =  ()=>{
+//         console.log(this.name);
+//     }
+//     b();
+   
+//     }
+// };
+// obj1.show();
+
+// let a = "MY NAME IS ATISHAY"
+// let b = a.split(" ");
+// console.log(b);
+//  const arr= ["helllo","is","jss","jio"];
+//  console.log(arr.toString());
+//  console.log(arr.join("*"));
+// console.log(arr.at(1));
+// console.log(arr.unshift("hey"));
+// console.log(arr.push("hey"));
+// console.log(arr.shift());
+// console.log(arr.pop());
+// console.log(arr);
+// console.log(arr.slice(0,3));
+const arr= [1,2,[3,[[5,6]],[7],8,9]];
+// const arr1=[1,2,3,4,5];
+// console.log(arr.splice(2,1,"jaa","Atishsy"));
+// // console.log(arr);
+// console.log(Array.isArray(arr));
+// console.log(arr.concat(arr1));
+// console.log(delete arr[1]);
+// console.log(arr);
+// console.log(arr.splice(2,1));
+// console.log(arr);
+// console.log(arr.copyWithin(1,3,5));
+// console.log(arr);
+console.log(arr.flat(2));
